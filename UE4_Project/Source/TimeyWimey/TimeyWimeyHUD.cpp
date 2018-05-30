1cc25603660e5cd08d2c6f8093bb674fea58eb86
Martin Larsson <martlars@playgroundsquad.com>
2018-05-30 13:13:49 +0200

    rearrange texture structure. Change accordingly in HUD.cpp

--- UE4_Project/Source/TimeyWimey/TimeyWimeyHUD.cpp
+++ UE4_Project/Source/TimeyWimey/TimeyWimeyHUD.cpp
@@ -10,7 +10,7 @@
 ATimeyWimeyHUD::ATimeyWimeyHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Textures/HUD/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
