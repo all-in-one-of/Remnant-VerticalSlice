34f4186896f0fe36c6734e318d23ef8956c4aaa0
Martin Larsson <martlars@playgroundsquad.com>
2018-05-30 14:27:48 +0200

    Remove unused input callbacks

--- UE4_Project/Source/TimeyWimey/PlayerFPP_Character.cpp
+++ UE4_Project/Source/TimeyWimey/PlayerFPP_Character.cpp
@@ -6,6 +6,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/InputSettings.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
@@ -47,17 +48,10 @@ void APlayerFPP_Character::SetupPlayerInputComponent(class UInputComponent* Play
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// Bind movement events
 	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerFPP_Character::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerFPP_Character::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turn rate" is for devices that we choose to treat as a rate of change, such as an analog joystick
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &APlayerFPP_Character::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &APlayerFPP_Character::LookUpAtRate);
 }
 
 void APlayerFPP_Character::MoveForward(float Value)
@@ -76,16 +70,4 @@ void APlayerFPP_Character::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
 	}
-}
-
-void APlayerFPP_Character::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void APlayerFPP_Character::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
--- UE4_Project/Source/TimeyWimey/PlayerFPP_Character.h
+++ UE4_Project/Source/TimeyWimey/PlayerFPP_Character.h
@@ -40,18 +40,6 @@ protected:
 
 	/** Handles strafing movement, left and right */
 	void MoveRight(float Val);
-
-	/**
-	 * Called via input to turn at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate.
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
 	
 protected:
 	// APawn interface
